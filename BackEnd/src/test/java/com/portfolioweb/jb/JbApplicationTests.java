f2cf91e0621e1ca7b64a52f0cf5099215719a070
JoaquinB7 <107253042+JoaquinB7@users.noreply.github.com>
2022-08-26 17:51:41 +0200

    Apache, MySQL, XAMPP,

---
+++ BackEnd/src/test/java/com/portfolioweb/jb/JbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioweb.jb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
